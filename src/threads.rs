3c9059d47e4eee517e140e7d293c461e19554c53
Giovani Andrade <giovanidandrade@gmail.com>
2024-08-24 11:44:47 -0300

    minor tweak

--- src/threads.rs
+++ src/threads.rs
@@ -5,7 +5,7 @@ use std::thread::JoinHandle;
 
 /// Renders the image in a single thread, used mostly for debugging
 pub fn render(image_dimensions: Dimensions, camera: Camera, world: &World) -> PngTile {
-    camera.render(0, image_dimensions, TileCorner(0, 0), &world)
+    camera.render(0, image_dimensions, TileCorner(0, 0), world)
 }
 
 /// Given a camera, a world and a render function, uses as many cores available to render the scene
